ecd5ed4b065c1087ac494845472f060efb61a43b
Yidhir-sudo <64656379+Yidhir-sudo@users.noreply.github.com>
2025-11-02 22:42:12 +0100

    new exercises and exams correction + general refactor (#4)
    * update the package names + upgrade the the first exercices with light code and more complex cases check (#2)
    
    Co-authored-by: Yidhir <ymoudoub@tf1.fr>
    
    * update the package names + upgrade the the first exercices with light code and more complex cases check
    
    * new exercises and exams correction
    
    ---------
    
    Co-authored-by: Yidhir <ymoudoub@tf1.fr>

---
+++ src/main/scala/esgi/iabd/programmationFonctionnelle/bonus/Bonus.scala
@@ -0,0 +1,8 @@
+package esgi.iabd.programmationFonctionnelle.bonus
+
+object Bonus {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ src/main/scala/esgi/iabd/programmationFonctionnelle/seance8/CorrectionSeance8.scala
@@ -0,0 +1,8 @@
+package esgi.iabd.programmationFonctionnelle.seance8
+
+object CorrectionSeance8 {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
